7ed8909fa2b62160edc9e5058864b34f7c7a7538
Radviger <radviger@gmail.com>
2020-03-17 02:05:20 +0200

    Added MB_DEFAULT_DESKTOP_ONLY to win MessageBox parameters to remove broken title bar icon.

--- src/windows.rs
+++ src/windows.rs
@@ -4,16 +4,16 @@ pub fn create(title: &str, content: &str, icon_type: IconType) {
     use std::iter::once;
     use std::ptr::null_mut;
     use winapi::um::winuser::{
-        MessageBoxW, MB_ICONERROR, MB_ICONINFORMATION, MB_OK, MB_SYSTEMMODAL,
+        MessageBoxW, MB_ICONERROR, MB_ICONINFORMATION, MB_OK, MB_SYSTEMMODAL, MB_DEFAULT_DESKTOP_ONLY,
     };
 
     let lp_text: Vec<u16> = content.encode_utf16().chain(once(0)).collect();
     let lp_caption: Vec<u16> = title.encode_utf16().chain(once(0)).collect();
 
     let window_type = match icon_type {
-        IconType::Error => MB_OK | MB_ICONERROR | MB_SYSTEMMODAL,
-        IconType::Info => MB_OK | MB_ICONINFORMATION | MB_SYSTEMMODAL,
-        IconType::None => MB_OK | MB_SYSTEMMODAL,
+        IconType::Error => MB_OK | MB_ICONERROR | MB_SYSTEMMODAL | MB_DEFAULT_DESKTOP_ONLY,
+        IconType::Info => MB_OK | MB_ICONINFORMATION | MB_SYSTEMMODAL | MB_DEFAULT_DESKTOP_ONLY,
+        IconType::None => MB_OK | MB_SYSTEMMODAL | MB_DEFAULT_DESKTOP_ONLY,
     };
 
     unsafe {
